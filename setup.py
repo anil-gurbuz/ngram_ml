51efc2e52b024e54a7c08a28ec76d9d757b44b5c
GurbuzA <anil.gurbuz@newcrest.com.au>
2023-03-17 09:28:09 +1100

    Added NN module

--- setup.py
+++ setup.py
@@ -11,15 +11,16 @@ classifiers = [
 
 setup(
     name='ngram_ml',
-    version='0.0.1',
+    version='0.1.0',
     description='Basic python package for creating n-gram language models from text files',
+    long_description_content_type  = 'text/markdown',
     long_description=open('README.md').read(),
     url='https://github.com/anil-gurbuz/ngram_ml',
     author='Anil Gurbuz',
     author_email='anlgrbz91@gmail.com',
     license='MIT',
     classifiers=classifiers,
-    keywords=['NLP', 'ngram', 'MLE', 'Simple Language Model'],
+    keywords=['NLP', 'ngram', 'MLE', 'Simple Language Model', 'Neural Network'],
     packages=find_packages(),
     install_requires=['requests','numpy']
 )
